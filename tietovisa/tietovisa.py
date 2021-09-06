96ac36b0bd4c3aeb25de6433d37ce5f2c12f1b55
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2021-09-06 16:25:48 +0300

    Lisää tietovisatehtävä

---
+++ tietovisa/tietovisa.py
@@ -0,0 +1,24 @@
+def visaile(kysymykset):
+    for kysymys in kysymykset:
+        print(kysymys["kysymys"])
+        vastaus = input()
+        if vastaus == kysymys["vastaus"]:
+            print("oikein meni!")
+        else:
+            print("väärin meni =(")
+        print()
+
+
+if __name__ == "__main__":
+    kysymykset = [
+        {"kysymys": "Mikä on maailman painavin eläin?",
+         "vastaus": "sinivalas"},
+        {"kysymys": "Mikä on Suomen korkein tunturi?",
+         "vastaus": "Halti"},
+        {"kysymys": "Mikä on viikon kolmas päivä?",
+         "vastaus": "keskiviikko"},
+        {"kysymys": "Montako jalkaa on hämähäkillä (vastaa numerolla, esim 3)",
+         "vastaus": "8"},
+    ]
+
+    visaile(kysymykset)
