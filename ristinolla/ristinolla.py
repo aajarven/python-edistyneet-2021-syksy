3bf3256b75d7243108dd295693b6067513d2cdc9
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2021-09-06 16:06:53 +0300

    Mahdollista mielivaltaisen pelitilanteen tulostaminen

--- ristinolla/ristinolla.py
+++ ristinolla/ristinolla.py
@@ -5,5 +5,29 @@ def tulosta_tyhja_ruudukko():
     print("-+-+-")
     print(" | | ")
 
+def tulosta_ruudukko(pelitilanne):
+    print("{}|{}|{}".format(pelitilanne[0][0], pelitilanne[0][1], pelitilanne[0][2]))
+    print("-+-+-")
+    print("{}|{}|{}".format(pelitilanne[1][0], pelitilanne[1][1], pelitilanne[1][2]))
+    print("-+-+-")
+    print("{}|{}|{}".format(pelitilanne[2][0], pelitilanne[2][1], pelitilanne[2][2]))
+
+
 if __name__ == "__main__":
     tulosta_tyhja_ruudukko()
+    print()
+
+    pelitilanne = [
+        [" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "],
+    ]
+    tulosta_ruudukko(pelitilanne)
+    print()
+
+    pelitilanne = [
+        ["X", " ", "O"],
+        [" ", "X", " "],
+        [" ", " ", "O"],
+    ]
+    tulosta_ruudukko(pelitilanne)
